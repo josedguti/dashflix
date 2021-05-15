4da742a27a9c3048f1a57e7d72e5d734a18b0339
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-05-15 15:24:39 -0400

    added meta tag in index

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -4,7 +4,7 @@ const MovieList = (props) => {
     const FavoriteComponent = props.favoriteComponent;
     return (
         <>
-        {props.movies.map((movie, index) => (
+        {props.movies.map((movie) => (
         <div className='image-container d-flex justify-content-start m-3'>
             <img src={movie.Poster} alt='movie'></img>
             <div 
